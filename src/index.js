f69a70d8a4539b35793f5d883fc54bae1b7b278a
Devnol <pgkoutsoumanis@gmail.com>
2021-08-25 19:55:46 +0000

    move project list to separate file

--- src/index.js
+++ src/index.js
@@ -5,25 +5,7 @@ import App from './App';
 import Project from './Projects'
 import reportWebVitals from './reportWebVitals';
 
-import QuboRun from "./icons/QuboRun.png";
-import GeometrySlash from "./icons/GeometrySlash.png";
-
-const projects =  {
-  "QuboRun": {
-    "name": "QuboRun",
-    "description": "A cube running endlessly, trying to avoid obstacles.",
-    "lang": "C#, Mono, .NET, Unity",
-    "url": "https://github.com/Devnol/QuboRun",
-    "icon": QuboRun
-  },
-  "GeometrySlash": {
-    "name": "GeometrySlash",
-    "description": "A simple matching game I made along with Peter0x44.",
-    "lang": "C++, raylib",
-    "url": "https://github.com/Peter0x44/GeometrySlash",
-    "icon": GeometrySlash
-  }
-}
+import projects from './Projlist'
 
 ReactDOM.render(
   <React.StrictMode>
