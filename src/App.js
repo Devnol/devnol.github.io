846e6e4ab15a727fa664f4d3733689a37df38485
Alexander Johansen <odyssey346@disroot.org>
2021-08-26 18:31:57 +0200

    ok akis
    Signed-off-by: Alexander Johansen <odyssey346@disroot.org>

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ function App() {
         </div>
         {info}
         {/* TODO: Add a scroll prompt */}
-        <div class="scroll-prompt"><b id="scrolltext">scroll down</b></div>
+        <div class="scroll-prompt"><b id="scrollText">scroll down</b></div>
     </div>
   );
 }
