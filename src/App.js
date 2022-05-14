0b46ed5cf3ab57da4a53d500d357e082af0beab2
Devnol <pgkoutsoumanis@gmail.com>
2022-05-14 16:57:16 +0300

    Remove Hackropolis, add projectsegfault and matrix account
    Signed-off-by: Devnol <pgkoutsoumanis@gmail.com>

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Button from '@material-ui/core/Button';
 
 import github from './icons/github.svg'
 import twitter from './icons/twitter.svg'
-import hackclub from './icons/hackclub.svg'
 import youtube from './icons/youtube.svg'
 import discord from './icons/discord.svg'
+import matrix from './icons/matrix.svg'
 
 const currAge = new Date().getFullYear() - 2007; //determine age, where 2007 is the year of birth
 
@@ -16,14 +16,14 @@ const currAge = new Date().getFullYear() - 2007; //determine age, where 2007 is 
 const whoIAm = 
   <div id="Info" class="anim">
     <p>I am a {currAge} year-old student, born and raised in Greece.</p>
-    <p> I always liked  computers and loved learning about new technologies and playing around with both software and hardware.</p>
-    <p> I'm a member of the <a href="https://hackropolis.club" className="App-link">Hackropolis</a> club, part of <a href="https://hackclub.com" className="App-link">Hack Club</a>, a nonprofit network of high school coding clubs and makers around the world.</p>
+    <p> I've always liked  computers and loved learning about new technologies and playing around with both software and hardware.</p>
+    <p> I am the Athens datacenter admin of <a href='https://projectsegfau.lt' className='App-link'>Project Segfault</a>, hosting all sorts of games and decentralised services.</p>
   </div>;
 
 const whatIDo = 
   <div id="Info" class="anim">
     <p>I enjoy coding and learning new languages and tools on every new project.</p>
-    <p>I don't have a preferred language but I've tried several in projects such as games with Unity and C#, websites with ReactJS, scripts with Python and Discord bots with JavaScript.</p>
+    <p>I don't have a preferred language but I've tried several in projects such as games with Unity and C#, websites with ReactJS, Wii Homebrew, Arduino/RP2040 projects with C/C++ and Discord bots with JavaScript.</p>
     <p>I also like hardware design, playing with microcontrollers and designing circuit boards with KiCad.</p>
   </div>;
 
@@ -31,9 +31,9 @@ const contactMe =
   <div id="Info" class="anim" style={{textAlign: "center"}}>
     <div className="buttonArray">
       <Social img={discord}  alt="Join my Discord Server (or find me as Devnol#9366)" href="https://discord.gg/MXZKx7de6M"/>
+      <Social img={matrix}  alt="Find me on the Matrix universe" href="https://matrix.to/#/@devnol:projectsegfau.lt"/>
       <Social img={github}  alt="Follow me on Github" href="https://github.com/Devnol"/>
       <Social img={twitter} alt="Follow me on Twitter" href="https://twitter.com/PanosDevnol"/>
-      <Social img={hackclub}  alt="Check out my HackClub Scrapbook" href="https://scrapbook.hackclub.com/Devnol"/>
       <Social img={youtube}  alt="Subscribe to my YouTube channel for occasional streams" href="https://www.youtube.com/channel/UClmSnMrllvKpiZQ79XmqznQ"/>
     </div>
   </div>;
